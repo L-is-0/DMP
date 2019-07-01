6798ccbd8f51be93ad540089ae817d8fa2c4d393
Hazel Wang <hw19@it-innovation.soton.ac.uk>
2019-07-01 15:29:41 +0100

    [Feature]Connect to mongo db

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,25 @@
-import os
-
+import pymongo
 from flask import Flask
-
+from pymongo import MongoClient
 
 def create_app():
     app = Flask(__name__)
 
+    client = pymongo.MongoClient(
+        "mongodb://admin:Admin123@dmp-xdarb.mongodb.net/test?retryWrites=true&w=majority")
+    db = client.dmpdb
+
+    # db = client.dmpdb
+
     @app.route('/index')
     def index():
         return 'Welcome, This is Data Management Platform'
+
+    @app.route('/users')
+    def get_all_users():
+        collection = db['users']
+        online_users = collection
+        return "Retrieves all online users"
 
     from . import auth
     app.register_blueprint(auth.bp)
